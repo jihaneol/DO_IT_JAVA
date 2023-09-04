fd302e8a9d02d2e451ae61435c072729222b4628
ji <6531z@naver.com>
2023-09-04 23:44:41 +0900

    feat : 순열

---
+++ 조합/src/Main_순열의순서_1722.java
@@ -0,0 +1,12 @@
+package 조합.src;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class Main_순열의순서_1722 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}

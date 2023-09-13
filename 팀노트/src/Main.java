1e9eee86df61067be5fd5e8c4d2513c9284bdcda
ji <6531z@naver.com>
2023-09-14 05:25:37 +0900

    feat : 트리의지름

--- 팀노트/src/Main.java
+++ 팀노트/src/Main.java
@@ -1,25 +1,31 @@
 package 팀노트.src;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-
-
-        System.out.println(Integer.compare(1,2)); // 오름 -1
-        System.out.println(Integer.compare(1,1)); // 0
-        System.out.println(Integer.compare(2,1)); // 내림 1
-
-        System.out.println(("02151".charAt(1)-'0')+5);
-        List<Integer> list  = new ArrayList<>();
-        list.size();
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int N = sc.nextInt()/4;
+        for(int i=0; i<N; i++){
+            sb.append("long ");
+        }
+        sb.append("int");
+        System.out.println(sb);
 
     }
 
 }
+//
+//        System.out.println(Integer.compare(1,2)); // 오름 -1
+//        System.out.println(Integer.compare(1,1)); // 0
+//        System.out.println(Integer.compare(2,1)); // 내림 1
+//
+//        System.out.println(("02151".charAt(1)-'0')+5);
+//        List<Integer> list  = new ArrayList<>();
+//        list.size();20
+//
+//        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());

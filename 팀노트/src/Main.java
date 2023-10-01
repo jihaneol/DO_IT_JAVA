0e3892f3531e8a735602edfc805bdb8533314b42
ji <6531z@naver.com>
2023-10-02 05:26:28 +0900

    feat : 크로아티아

--- 팀노트/src/Main.java
+++ 팀노트/src/Main.java
@@ -1,48 +1,23 @@
 package 팀노트.src;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        String s = br.readLine();
+        String[] croa = {"c=","c-","dz=","d-","lj","nj","s=","z="};
 
-        int N = Integer.valueOf(st.nextToken());
-        int M = Integer.valueOf(st.nextToken());
-
-        int[] arr = new int[N+1];
-
-        for(int c=0; c<M; c++){
-            st = new StringTokenizer(br.readLine());
-            int i = Integer.valueOf(st.nextToken());
-            int j = Integer.valueOf(st.nextToken());
-            int k = Integer.valueOf(st.nextToken());
-
-            for(int x=i;x<=j;x++){
-                arr[x] = k;
+        for(String cro : croa){
+            if(s.contains(cro)){
+                s = s.replace(cro,"@");
             }
         }
-        StringBuilder sb = new StringBuilder();
-        for(int i=1; i<=N; i++){
-            sb.append(arr[i]).append(" ");
-        }
-        System.out.println(sb);
-
+        System.out.println(s);
+        System.out.println(s.length());
 
     }
 
 }
-//
-//        System.out.println(Integer.compare(1,2)); // 오름 -1
-//        System.out.println(Integer.compare(1,1)); // 0
-//        System.out.println(Integer.compare(2,1)); // 내림 1
-//
-//        System.out.println(("02151".charAt(1)-'0')+5);
-//        List<Integer> list  = new ArrayList<>();
-//        list.size();20
-//
-//        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
